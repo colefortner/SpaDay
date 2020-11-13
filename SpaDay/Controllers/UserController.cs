ba6049f2c71e83216dc1074332ce8a132081a29c
colefortner <colefortner@gmail.com>
2020-11-12 23:42:24 -0500

    * User.cs: * Add.cshtml: * Index.cshtml: * UserController.cs:
    * SpaDay.csproj: Chapter 14 studio

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //ViewBag.title = "what";
+            //ViewBag.user = farts.username;          
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/User/Add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        //public IActionResult SubmitAddUserForm(string username, string email, string password, string verify)
+        {
+            ViewBag.name = newUser.Username;
+            ViewBag.email = newUser.Email;
+            ViewBag.date = newUser.Date;
+            if (newUser.Password == verify)
+            {
+                //ViewBag.name = newUser.Username;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Form could not submit, the passwords do not match";
+                //return Redirect("/User/Add"); This did not work
+                return View("Add");
+
+            }
+
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Date { get; }
+
+        public User()
+        {
+            Date = DateTime.Now;
+        }
+
+        public User(string username, string email, string password): this()
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return Username;
+        }
+    }
+}
